849d7e29b2bd2577cfccc4ca3b5a24de0355ce4c
script kiddie <anish.chittu2007@gmail.com>
2024-09-19 22:44:24 +0200

    finished compiler

--- compiler/main.go
+++ compiler/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"sigma_compiler/lexer"
 	"sigma_compiler/parser"
+	"strings"
 )
 var Req = []string{
 		"sigma main",
@@ -12,28 +15,96 @@ var Req = []string{
 }
 
 func main(){
-	fmt.Println(len(os.Args))
-	fmt.Println(os.Args[0])
-	fmt.Println(os.Args[1])
-	fmt.Println(os.Args[2])
+	if len(os.Args) != 3{
+		fmt.Println("Usage: sigma <flag> <file>")
+        return
+	}
+	flag := os.Args[1]
+	filename := os.Args[2]
+
+	switch flag {
+		case "build":
+            build(filename)
+        default:
+            fmt.Println("Invalid flag. Use 'build'")
+	}
+}
+
+func buildGoFilr(filename string) error{
+	ext := filepath.Ext(filename)
+	if ext!= ".go"{
+        return fmt.Errorf("failed to compile")
+    }
+
+	cmd := exec.Command("go" ,"build", filename)
+	err := cmd.Run()
+	if err!= nil {
+        return fmt.Errorf("failed to compile")
+    }
+	
+	return nil
+}
+
+func isProgramInstalled(program string) bool {
+	_, err := exec.LookPath(program)
+	return err == nil
+}
 
 
+func checkfile(filename string) (newfile string, ok bool){
+	ext := filepath.Ext(filename)
+	if ext!= ".sigma"{
+        return "", false
+    }
 
-	file ,err := os.ReadFile("D:\\sigma\\tests\\out\\test2.sigma")
+	_, err := os.Stat(filename)
+	if os.IsNotExist(err){
+        return "", false
+    }
+
+	ff := strings.TrimSuffix(filename, ".sigma")
+	return ff + ".go", true
+}
+
+func build(filename string){
+	if !isProgramInstalled("go"){
+        fmt.Println("go compiler is not installed")
+        return
+    }
+
+	newfile, ok := checkfile(filename)
+	if !ok{
+		fmt.Println("file does not exist")
+        return
+	}
+
+	file ,err := os.ReadFile(filename)
 	if err!= nil {
         fmt.Printf("Error reading file: %v\n", err)
         return
     }
 	
     text := string(file)
-	ok := parser.Parser(text, Req)
+	ok = parser.Parser(text, Req)
 	if ok{
 		compiledtext := lexer.Words(text)
-		os.WriteFile("/sigma/tests/out/test2.go", []byte(compiledtext), 0644)
+		err := os.WriteFile(newfile, []byte(compiledtext), 0644)
+		if err!= nil {
+            fmt.Printf("Error writing file: %v\n", err)
+            return
+        }
+
+		defer os.Remove(newfile)
+		err = buildGoFilr(newfile)
+		if err != nil{
+			fmt.Println(err)
+			return
+        }
+
+		fmt.Println("Compilation successful")
 	}else{
 		fmt.Println("error missing: 'sigma main' or 'skibidi main#@")
 	}
 
 }
 
-//sigma build main.sigma
