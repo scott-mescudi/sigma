a50b511672dea526a1cb8271b89bcfa7ce1ad190
script kiddie <anish.chittu2007@gmail.com>
2024-09-20 23:58:41 +0200

    added support for threads and networking

--- src/main.go
+++ src/main.go
@@ -9,39 +9,40 @@ import (
 	"sigma_compiler/parser"
 	"strings"
 )
+
 var Req = []string{
-		"sigma main",
-		"skibidi main#@",
+	"sigma main",
+	"skibidi main#@",
 }
 
-func main(){
-	if len(os.Args) != 3{
+func main() {
+	if len(os.Args) != 3 {
 		fmt.Println("Usage: sigma <flag> <file>")
-        return
+		return
 	}
 	flag := os.Args[1]
 	filename := os.Args[2]
 
 	switch flag {
-		case "build":
-            build(filename)
-        default:
-            fmt.Println("Invalid flag. Use 'build'")
+	case "build":
+		build(filename)
+	default:
+		fmt.Println("Invalid flag. Use 'build'")
 	}
 }
 
-func buildGoFilr(filename string) error{
+func buildGoFilr(filename string) error {
 	ext := filepath.Ext(filename)
-	if ext!= ".go"{
-        return fmt.Errorf("failed to compile")
-    }
+	if ext != ".go" {
+		return fmt.Errorf("failed to compile")
+	}
 
-	cmd := exec.Command("go" ,"build", filename)
+	cmd := exec.Command("go", "build", filename)
 	err := cmd.Run()
-	if err!= nil {
-        return fmt.Errorf("failed to compile")
-    }
-	
+	if err != nil {
+		return fmt.Errorf("failed to compile")
+	}
+
 	return nil
 }
 
@@ -50,61 +51,59 @@ func isProgramInstalled(program string) bool {
 	return err == nil
 }
 
-
-func checkfile(filename string) (newfile string, ok bool){
+func checkfile(filename string) (newfile string, ok bool) {
 	ext := filepath.Ext(filename)
-	if ext!= ".sigma"{
-        return "", false
-    }
+	if ext != ".sigma" {
+		return "", false
+	}
 
 	_, err := os.Stat(filename)
-	if os.IsNotExist(err){
-        return "", false
-    }
+	if os.IsNotExist(err) {
+		return "", false
+	}
 
 	ff := strings.TrimSuffix(filename, ".sigma")
 	return ff + ".go", true
 }
 
-func build(filename string){
-	if !isProgramInstalled("go"){
-        fmt.Println("go compiler is not installed")
-        return
-    }
-
-	newfile, ok := checkfile(filename)
-	if !ok{
-		fmt.Println("file does not exist")
-        return
+func build(filename string) {
+	if !isProgramInstalled("go") {
+		fmt.Println("go compiler is not installed")
+		return
 	}
 
-	file ,err := os.ReadFile(filename)
-	if err!= nil {
-        fmt.Printf("Error reading file: %v\n", err)
-        return
-    }
-	
-    text := string(file)
+	newfile, ok := checkfile(filename)
+	if !ok {
+		fmt.Println("file does not exist")
+		return
+	}
+
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Printf("Error reading file: %v\n", err)
+		return
+	}
+
+	text := string(file)
 	ok = parser.Parser(text, Req)
-	if ok{
+	if ok {
 		compiledtext := lexer.Words(text)
 		err := os.WriteFile(newfile, []byte(compiledtext), 0644)
-		if err!= nil {
-            fmt.Printf("Error writing file: %v\n", err)
-            return
-        }
+		if err != nil {
+			fmt.Printf("Error writing file: %v\n", err)
+			return
+		}
 
 		defer os.Remove(newfile)
 		err = buildGoFilr(newfile)
-		if err != nil{
+		if err != nil {
 			fmt.Println(err)
 			return
-        }
+		}
 
 		fmt.Println("Compilation successful")
-	}else{
+	} else {
 		fmt.Println("error missing: 'sigma main' or 'skibidi main#@")
 	}
 
 }
-
